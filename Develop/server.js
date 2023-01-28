72e50d707479a4e1cc80ab725f19151a38ecbb5b
Garrett <garrettleegrahamanderson@gmail.com>
2023-01-28 17:09:50 -0500

    refactored the html routes to be modular. created routes folder with htmlRoutes.js and exported router. able to navigate to both end points

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path');
 const fs = require('fs')
 const noteData = require('./db/db.json')
+const htmlRouter = require('./routes/htmlRoutes')
 const uuid = require('./helpers/uuid');
 // const { parse } = require('path');
 const PORT = 3000
@@ -11,14 +12,7 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-  });
-  
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+app.use('/', htmlRouter)
 
 app.get('/api/notes', (req, res) => {
   console.info(`${req.method} request made`);
