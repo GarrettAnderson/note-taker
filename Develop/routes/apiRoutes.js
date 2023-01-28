0e051fcdfb172c04a8c75ba60af417b7d5233b13
Garrett <garrettleegrahamanderson@gmail.com>
2023-01-28 18:01:37 -0500

    now refactored and included the POST and DELETE requests. able to add a notes and deltete a note on the front end and in back end

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,1 +1,80 @@
-const notes = require('express').Router()
+const notes = require('express').Router();
+const fs = require('fs')
+// const noteData = require('./db/db.json')
+const uuid = require('../helpers/uuid')
+
+notes.get('/notes', (req, res) => {
+    console.info(`${req.method} request made`);
+  
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+      if(err) {
+        console.log(err)
+      } else {
+        res.status(200).send(data);
+      }
+    })
+    // console.log(noteData)
+    // res.send(noteData) 
+  });
+  
+
+
+notes.post('/notes', (req, res) => {
+    console.table(`${req.body.title} ${req.body.text} POST call received`)
+    const { title, text } = req.body
+    if (title && text) {
+      const newNote = {
+        title,
+        text,
+        id: uuid()
+      }
+      console.log(newNote)
+      fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if(err) {
+          console.log(err)
+        } else {
+          const parsedNotes = JSON.parse(data)
+          parsedNotes.push(newNote)
+          // console.log(parsedNotes) 
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) => {
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated reviews!')
+                res.json(parsedNotes)
+            }
+          )
+        }
+      })
+      
+      // const response = {
+      //   status: 'success',
+      //   body: newNote
+      // }
+  
+      // console.log(response)
+      // res.status(200).json(response) 
+    } else {
+      res.status(500).json('Error in posting review');
+  
+    }
+  })
+  
+  
+  notes.delete('/notes/:id', (req, res) => {
+    let noteId = req.params.id
+    console.info(`${noteId} called to remove note`)
+    // read notes from db/db.json
+    let db = JSON.parse(fs.readFileSync('./db/db.json'))
+    console.log(db)
+    let deleteNotes = db.filter((note) => note.id !== req.params.id)
+    console.log(deleteNotes)
+  
+    fs.writeFileSync('./db/db.json', JSON.stringify(deleteNotes))
+    res.json(deleteNotes)
+  })
+  
+
+  module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path');
 const fs = require('fs')
-const noteData = require('./db/db.json')
+const apiRouter = require('./routes/apiRoutes')
 const htmlRouter = require('./routes/htmlRoutes')
 const uuid = require('./helpers/uuid');
 // const { parse } = require('path');
@@ -12,79 +12,8 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/', htmlRouter)
-
-app.get('/api/notes', (req, res) => {
-  console.info(`${req.method} request made`);
-
-  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-    if(err) {
-      console.log(err)
-    } else {
-      res.status(200).send(data);
-    }
-  })
-  // console.log(noteData)
-  // res.send(noteData) 
-})
-
-app.post('/api/notes', (req, res) => {
-  console.table(`${req.body.title} ${req.body.text} POST call received`)
-  const { title, text } = req.body
-  if (title && text) {
-    const newNote = {
-      title,
-      text,
-      id: uuid()
-    }
-    console.log(newNote)
-    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-      if(err) {
-        console.log(err)
-      } else {
-        const parsedNotes = JSON.parse(data)
-        parsedNotes.push(newNote)
-        // console.log(parsedNotes) 
-        fs.writeFile(
-          './db/db.json',
-          JSON.stringify(parsedNotes, null, 4),
-          (writeErr) => {
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successfully updated reviews!')
-              res.json(parsedNotes)
-          }
-        )
-      }
-    })
-    
-    // const response = {
-    //   status: 'success',
-    //   body: newNote
-    // }
-
-    // console.log(response)
-    // res.status(200).json(response) 
-  } else {
-    res.status(500).json('Error in posting review');
-
-  }
-})
-
-
-app.delete('/api/notes/:id', (req, res) => {
-  let noteId = req.params.id
-  console.info(`${noteId} called to remove note`)
-  // read notes from db/db.json
-  let db = JSON.parse(fs.readFileSync('./db/db.json'))
-  console.log(db)
-  let deleteNotes = db.filter((note) => note.id !== req.params.id)
-  console.log(deleteNotes)
-
-  fs.writeFileSync('./db/db.json', JSON.stringify(deleteNotes))
-  res.json(deleteNotes)
-})
-
+app.use('/api', apiRouter);
+app.use('/', htmlRouter);
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
